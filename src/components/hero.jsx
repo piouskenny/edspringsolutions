d20e150ba2481f779f3157833b7778644996182b
piouskenny <kehindeu13@gmail.com>
2023-02-23 00:23:17 +0100

    added nav bar

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,9 +4,9 @@ const Hero = () => {
   return (
     <>
     <Navbar />
-      <div className="bg-blue-900 w-full h-2/4 text-white md:grid md:grid-cols-2 md:px-32 md:pb-20 md:pt-20 pt-24 pb-12 px-6 ">
+      <div className="bg-blue-900 w-full h-2/4 text-white md:grid md:grid-cols-2 md:px-32 md:pb-10 md:pt-10 pt-24 pb-12 px-6 ">
       
-        <div className="md:col-span-1 ">
+        <div className="md:col-span-1 md:mt-12 ">
             <h1 className="text-3xl md:text-5xl font-medium">The Perfect Place for Education Consult</h1>
 
             <p className="text-gray-400 md:mt-6 mt-4 text-md">Lorem ipsum dolor sit amet velit illum asperiores mollitia odit eligendi.</p>
@@ -15,7 +15,9 @@ const Hero = () => {
                 <button className="bg-blue-400 px-4 mt-6 py-2 rounded-lg md:rounded-full hover:bg-white hover:text-blue-400">Contact Us &rarr;</button>
             </a>
         </div>
-        <div className="md:col-span-1"></div>
+        <div className="md:col-span-1">
+          <img src="https://img.freepik.com/free-photo/attractive-african-businessman-glasses-suit-holding-papers_273609-9306.jpg?w=740&t=st=1677107850~exp=1677108450~hmac=f64e30c0d6c8699678271093f9d47bbe5f1eb4439644e000eda27ff2555e697c" className="md:rounded-full pt-12 md:pt-0 rounded-lg" alt="" />
+        </div>
       </div>
     </>
   );
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -5,28 +5,93 @@ const Services = () => {
         id="services"
         className="bg-blue-900  text-white md:mt-6 md:px-32 w-full md:py-10 md:mb-10 py-6 mt-2 "
       >
-        <h1 className="text-white text-center text-2xl">Services</h1>
+        <h1 className="text-white text-center text-2xl mb-6">Services</h1>
         <div className="md:grid grid-cols-4 gap-4">
           <div className="text-center col-span-1 bg-blue-700 rounded-md md:rounded-xl mx-6 md:mx-0 my-4 md:my-0">
             <div className="py-10">
+              <div className="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819"
+                  />
+                </svg>
+              </div>
+
               <h1 className="md:py-2">Dolor</h1>
               <p className="text-gray-300">Lorem ipsum dolor sit.</p>
             </div>
           </div>
           <div className="col-span-1 text-center bg-blue-700 rounded-md md:rounded-xl mx-6 md:mx-0 my-4 md:my-0">
             <div className="py-10">
+              <div className="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
+                  />
+                </svg>
+              </div>
               <h1 className="md:py-2">Dolor</h1>
               <p className="text-gray-300">Lorem ipsum dolor sit.</p>
             </div>
           </div>
           <div className="text-center col-span-1 bg-blue-700 rounded-md md:rounded-xl mx-6 md:mx-0 my-4 md:my-0">
             <div className="py-10">
+              <div className="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
               <h1 className="md:py-2">Dolor</h1>
               <p className="text-gray-300">Lorem ipsum dolor sit.</p>
             </div>
           </div>
           <div className="text-center col-span-1 bg-blue-700 rounded-md md:rounded-xl mx-6 md:mx-0 my-4 md:my-0">
             <div className="py-10">
+              <div className="flex justify-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
+                  />
+                </svg>
+              </div>
               <h1 className="md:py-2">Dolor</h1>
               <p className="text-gray-300">Lorem ipsum dolor sit.</p>
             </div>
