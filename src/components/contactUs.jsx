cd37af9415271757b638ab5233e2a94464bba790
piouskenny <kehindeu13@gmail.com>
2023-02-23 12:25:54 +0100

    removed contact functionality contact

--- src/components/contactUs.jsx
+++ src/components/contactUs.jsx
@@ -1,45 +1,4 @@
-// import React, { useState, useRef } from "react";
-// import emailjs from "@emailjs/browser";
-
 const ContactUs = () => {
-  // const [name, setName] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [message, setMessage] = useState("");
-
-  // const handleNameChange = (e) => {
-  //   setName(e.target.value);
-  // };
-
-  // const handleEmailChange = (e) => {
-  //   setEmail(e.target.value);
-  // };
-
-  // const handleMessageChange = (e) => {
-  //   setMessage(e.target.value);
-  // };
-
-  // const form = useRef();
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-
-  //   emailjs
-  //     .sendForm(
-  //       "service_oulbj9u",
-  //       "template_btrlqfg",
-  //       form.current,
-  //       "gaFe6kiCuY4rt3oxD"
-  //     )
-  //     .then(
-  //       (result) => {
-  //         console.log(result.text);
-  //       },
-  //       (error) => {
-  //         console.log(error.text);
-  //       }
-  //     );
-  // };
-
   return (
     <div className="max-w-7xl mx-auto sm:px-6 lg:px-8  md:px-32 px-10 shadow-gray-900 shadow-2xl py-6 md:py-12">
       <div className="max-w-3xl mx-auto">
@@ -48,7 +7,6 @@ const ContactUs = () => {
         </h2>
 
         <form
-        //  onSubmit={handleSubmit}
           className="mt-6">
           <div>
             <label
@@ -62,8 +20,6 @@ const ContactUs = () => {
                 type="text"
                 name="user_name"
                 id="name"
-                // value={name}
-                // onChange={handleNameChange}
                 className="py-3 px-4 block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md"
                 placeholder="Enter your name"
               />
@@ -81,8 +37,6 @@ const ContactUs = () => {
                 type="email"
                 name="user_email"
                 id="email"
-                // value={email}
-                // onChange={handleEmailChange}
                 className="py-3 px-4 block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md"
                 placeholder="Enter your email"
               />
@@ -100,8 +54,6 @@ const ContactUs = () => {
                 name="message"
                 id="message"
                 rows="5"
-                // value={message}
-                // onChange={handleMessageChange}
                 className="py-3 px-4 block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md"
                 placeholder="Enter your message"
               ></textarea>
