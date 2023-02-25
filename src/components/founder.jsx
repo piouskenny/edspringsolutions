3dcc5980bfd816b4fe4787f36372f13e216cd66b
piouskenny <kehindeu13@gmail.com>
2023-02-25 10:06:56 +0100

    testing

--- src/components/founder.jsx
+++ src/components/founder.jsx
@@ -1,4 +1,5 @@
 import DrTaiwo from "../assets/portfolios/drTaiwo.jpeg";
+import { Link } from "react-router-dom";
 
 const Founder = () => {
   return (
@@ -15,7 +16,7 @@ const Founder = () => {
             <img src={DrTaiwo} alt="Founders Image" className="h-full w-full" />
           </div>
           <div className="lg:col-span-1 lg:mt-10">
-            <p className="md:text-xl  text-gray-600 text-lg mt-12">
+            <p className="md:text-xl font-light text-gray-600 text-lg mt-12">
               Dr. Emmanuel Taiwo is an education leader and consultant with over
               17 years of dedicated experience promoting excellence and equity
               in education for all students. He possesses a broad range of
@@ -23,17 +24,13 @@ const Founder = () => {
               teaching multilingual learners, culturally sustaining pedagogy,
               Multi-Tiered System of Supports (MTSS), behavior supports and
               interventions, differentiated instruction, Universal Design for
-              Learning (UDL), school leadership, and compliance. Having worked
-              in both K–12 and higher education settings, Dr. Taiwo is deeply
-              committed to empowering stakeholders in education to achieve
-              equitable learning outcomes for all students, with a focus on
-              driving systemic change and creating opportunities for all
-              students to flourish. As a passionate advocate for inclusive
-              education, Dr. Taiwo has made significant contributions to the
-              field through his research, publications, and numerous
-              presentations at national and international conferences. He is a
-              member of several professional organizations and holds a doctorate
-              in Special Education from John Hopkins University, Baltimore, USA.
+              Learning (UDL), school leadership, and compliance.
+
+              <Link to="/aboutMore/#founder" >
+                <button className="block bg-white px-4 mt-6 py-2 border-lg border border-black rounded-lg md:rounded-xl text-black hover:bg-white hover:text-blue-400">
+                  About Dr. Emmanuel Taiwo &rarr;
+                </button>
+              </Link>
             </p>
           </div>
         </div>
